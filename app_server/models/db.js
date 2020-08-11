df617fda4d7f8177a3b7456712003d52081af2a0
jazzzdima <dimag.concept@gmail.com>
2020-08-11 17:23:35 +0300

    Add database support

---
+++ app_server/models/db.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const credentials = require('../../credentials');
+const readline = require('readline');
+
+mongoose.connect(
+	credentials.mongo.development.connectionString, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	});
+
+mongoose.connection
+	.on('open', () => {
+		console.log('Mongoose connection open');
+	})
+	.on('error', err => {
+		console.log(`Mongoose connection error ${err.message}`);
+	})
+	.on('disconnected', () => {
+		console.log('Mongoose disconnected');	
+	});
+
+let grasefoolShutdown = function(msg, callback){
+	mongoose.connection.close(() => {
+		console.log('Mongoose disconnected throught ' + msg);
+		callback();
+	})
+};
+
+if (process.platform === 'win32'){
+	let rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+	});
+	rl.on('SIGINT', () => {
+		process.emit('SIGINT');
+	});
+}
+
+process.once('SIGUSR2', () => {
+	grasefoolShutdown('Nodemon restart', () => {
+		process.kill(process.pid, 'SIGUSR2');
+	});
+});
+
+process.on('SIGINT', () => {
+	grasefoolShutdown('App termination', () => {
+		process.exit(0);
+	});
+});
+
+process.on('SIGTERM', () => {
+	grasefoolShutdown('Heroku app shutdown', () => {
+		process.exit(0);
+	});
+});
+
---
+++ credentials.js
@@ -0,0 +1,10 @@
+module.exports = {	
+	mongo: {
+		development: {
+			connectionString: 'mongodb+srv://dbUser:789789@cluster0.o637q.mongodb.net/Cluster0?retryWrites=true&w=majority',
+		},
+		production: {
+			connectionString: 'mongodb+srv://dbUser:789789@cluster0.o637q.mongodb.net/Cluster0?retryWrites=true&w=majority',
+		},
+	},
+};
